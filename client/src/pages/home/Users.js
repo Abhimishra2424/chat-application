72dc32b08d5cf4f9f838fb83db6c29bea9a0a150
Abhishek <abmishra700@gmail.com>
2022-05-23 09:30:43 +0530

    Sending New Messages

--- client/src/pages/home/Users.js
+++ client/src/pages/home/Users.js
@@ -55,7 +55,13 @@ export default function Users() {
             dispatch({ type: 'SET_SELECTED_USER', payload: user.username })
           }
         >
-          <Image src={user.imageurl} className="user-image" />
+          <Image
+            src={
+              user.imageurl ||
+              'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'
+            }
+            className="user-image"
+          />
           <div className="d-none d-md-block ml-2">
             <p className="text-success">{user.username}</p>
             <p className="font-weight-light">
