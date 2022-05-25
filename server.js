e461b8d7bfdaa89ee0f496ccab2aabb28b3d8415
Abhishek <abmishra700@gmail.com>
2022-05-25 18:35:18 +0530

    prepard code for deployment

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const { ApolloServer } = require("apollo-server");
 
+require("dotenv").config();
+
 const { sequelize } = require("./models");
 
 const resolvers = require("./graphql/resolvers");
 const typeDefs = require("./graphql/typeDefs");
 
-const  contextMiddleware = require("./util/contextMiddleware");
+const contextMiddleware = require("./util/contextMiddleware");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   csrfPrevention: true,
-  context: contextMiddleware // THIS IS ACCESSIBLE FROM THE CLIENT like req in express
+  context: contextMiddleware, // THIS IS ACCESSIBLE FROM THE CLIENT like req in express
+  subscriptions: { path: "/" },
 });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
+server.listen().then(({ url, subscriptionsUrl }) => {
   console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀  subscriptions ready at ${subscriptionsUrl}`);
   sequelize
     .authenticate()
     .then(() => console.log("Connection has been established successfully."))
